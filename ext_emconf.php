79b41ee1ba53bad22aa616f87aba5f7c4b28cd75
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2017-12-04 10:51:01 +0100

    New release

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,33 +22,33 @@
  */
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'GS Library',
-	'description' => 'GS Library is a collection of utility functions, base classes and other templates for the usage in other extensions.',
-	'version' => '0.0.2',
-	'category' => 'misc',
-	'constraints' => [
-		'depends' => [
-			'php' => '5.6.0-0.0.0',
-			'typo3' => '6.2.0-8.9.99'
-		],
-		'conflicts' => [],
-		'suggests' => []
-	],
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'author' => 'Simon Gilli',
-	'author_email' => 'typo3@gilbertsoft.org',
-	'author_company' => 'Gilbertsoft',
-	'autoload' => [
-		'psr-4' => [
-			'Gilbertsoft\\Lib\\' => 'Classes'
-		]
-	],
-	'autoload-dev' => [
-		'psr-4' => [
-			'Gilbertsoft\\Lib\\Tests\\' => 'Tests'
-		]
-	]
+    'title' => 'GS Library',
+    'description' => 'GS Library is a collection of utility functions, base classes and other templates for the usage in other extensions.',
+    'version' => '0.0.3',
+    'category' => 'misc',
+    'constraints' => [
+        'depends' => [
+            'php' => '5.6.0-0.0.0',
+            'typo3' => '6.2.0-8.9.99'
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ],
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Simon Gilli',
+    'author_email' => 'typo3@gilbertsoft.org',
+    'author_company' => 'Gilbertsoft',
+    'autoload' => [
+        'psr-4' => [
+            'Gilbertsoft\\Lib\\' => 'Classes'
+        ]
+    ],
+    'autoload-dev' => [
+        'psr-4' => [
+            'Gilbertsoft\\Lib\\Tests\\' => 'Tests'
+        ]
+    ]
 ];
