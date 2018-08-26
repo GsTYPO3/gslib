3c596493030f71c8184114f374126315fb16ce29
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2018-08-26 17:44:04 +0200

    New release

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'GS Library',
     'description' => 'GS Library is a collection of utility functions, base classes and other templates for the usage in other extensions.',
-    'version' => '0.0.7',
+    'version' => '0.0.8',
     'category' => 'misc',
     'constraints' => [
         'depends' => [
